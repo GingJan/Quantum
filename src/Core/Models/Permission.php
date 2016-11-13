948ab5896792fcdcf7c9bd0f66fa05691093fe32
zjien <694396727@qq.com>
2016-11-13 15:23:24 +0800

    move methods to trait

--- src/Core/Models/Permission.php
+++ src/Core/Models/Permission.php
@@ -1,13 +1,18 @@
 <?php
 namespace Zjien\Quantum\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Config;
 use Zjien\Quantum\Contracts\PermissionContract;
+use Zjien\Quantum\Traits\PermissionTrait;
 
-class Permission extends Quantum implements PermissionContract
+class Permission extends Model implements PermissionContract
 {
-    const STATUS_CLOSING = 0;
-    const STATUS_OPENING = 1;
+    use PermissionTrait;
+
+    //resource status
+    const STATUS_CLOSING = 1;
+    const STATUS_OPENING = 0;
 
     /**
      * The Permission table.
@@ -18,32 +23,4 @@ class Permission extends Quantum implements PermissionContract
 
     protected $fillable = ['name', 'verb', 'uri', 'display_name', 'description', 'status'];
 
-    public function __construct(array $attributes = [])
-    {
-        parent::__construct($attributes);
-        $this->table = Config::get('config.database.tables.permission');
-    }
-
-    /**
-     * The "booting" method of the model.
-     *
-     * @return void
-     */
-    public static function boot()
-    {
-        parent::boot();
-
-        static::deleting(function($permission) {
-            $permission->roles()->detach();
-        });
-    }
-    /**
-     * Many to Many relations with the role model
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function roles()
-    {
-        return $this->belongsToMany(Config::get('config.model.role'), Config::get('config.database.tables.user_role_relation'));
-    }
 }
--- src/Core/Models/Role.php
+++ src/Core/Models/Role.php
@@ -1,113 +1,20 @@
 <?php
 namespace Zjien\Quantum\Models;
 
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Config;
 use Zjien\Quantum\Contracts\RoleContract;
+use Zjien\Quantum\Traits\RoleTrait;
 
-class Role extends Quantum implements RoleContract
+class Role extends Model implements RoleContract
 {
+    use RoleTrait;
+
     /**
      * The Role table.
      *
      * @var
      */
     protected $table;
-
-    public function __construct(array $attributes = [])
-    {
-        parent::__construct($attributes);
-        $this->table = Config::get('config.database.role');
-    }
-
-    /**
-     * The "booting" method of the model.
-     *
-     * @return void
-     */
-    public static function boot()
-    {
-        parent::boot();
-        static::deleting(function($role) {
-            $role->detachAllPermissions();
-            $role->detachAllUsers();
-
-            return true;
-        });
-    }
-
-    /**
-     * Many to Many relations with the user model
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function users()
-    {
-        return $this->belongsToMany(Config::get('auth.model'), Config::get('config.database.tables.user_role_relation'));
-    }
-
-    /**
-     * Many to Many relations with the user model
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function permissions()
-    {
-        return $this->belongsToMany(Config::get('config.model.permission'), Config::get('config.database.tables.role_permission_relation'));
-    }
-
-    /**
-     * Attach Permission for Role.
-     *
-     * @param array|Model|Collection $permissions
-     * @return void
-     */
-    public function attachPermission($permissions)
-    {
-        $this->permissions()->attach(static::normalize($permissions));
-    }
-
-    /**
-     * Update Permission for Role.
-     *
-     * @param array|Model|Collection $permissions
-     * @return array
-     */
-    public function updatePermission($permissions)
-    {
-        return $this->permissions()->sync(static::normalize($permissions));
-    }
-
-    /**
-     * Detach Permission for Role.
-     *
-     * @param array|Model|Collection $permissions
-     * @return int
-     */
-    public function detachPermission($permissions)
-    {
-        return $this->permissions()->detach(static::normalize($permissions));
-    }
-
-    /**
-     * Detach All Permissions of the Role
-     *
-     * @return int
-     */
-    public function detachAllPermissions()
-    {
-        return $this->permissions()->detach();
-    }
-
-    /**
-     * Detach All Users of the Role
-     *
-     * @return int
-     */
-    public function detachAllUsers()
-    {
-        return $this->user()->detach();
-    }
 
 }
